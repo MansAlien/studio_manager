c37ba07183f9a086c2b2c5e36ffe59fca821ce8d
MansAlien <mans012033945421@gmail.com>
2024-09-06 08:23:17 +0300

    create the urls of the inventory api

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,22 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import (
+    AttributeValueViewSet,
+    AttributeViewSet,
+    CaterogyViewSet,
+    ProductLineViewSet,
+    ProductViewSet,
+    SubCaterogyViewSet,
+)
+
 router = DefaultRouter()
+router.register(r'attribue', AttributeViewSet)
+router.register(r'attribue_value', AttributeValueViewSet)
+router.register(r'category', CaterogyViewSet)
+router.register(r'product', ProductViewSet)
+router.register(r'product_line', ProductLineViewSet)
+router.register(r'sub_category', SubCaterogyViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
