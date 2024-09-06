2659afa732fec5fca25bed754e217acdd33d6b9d
MansAlien <mans012033945421@gmail.com>
2024-09-06 18:54:42 +0300

    separate the views of the api and the normal views

---
+++ accounts/views_api.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import (
+    City,
+    Country,
+    Deduction,
+    Governorate,
+    JobTitle,
+    JobTitleHistory,
+    SalaryHistory,
+    UserProfile,
+)
+from .serializers import (
+    CitySerializer,
+    CountrySerializer,
+    DeductionSerializer,
+    GovernrateSerializer,
+    JobTitleHistorySerializer,
+    JobTitleSerializer,
+    SalaryHistorySerializer,
+    UserProfileSerializer,
+    UserSerializer,
+)
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    permission_classes = [IsAuthenticated]
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = [IsAuthenticated]
+
+class DeductionViewSet(viewsets.ModelViewSet):
+    queryset = Deduction.objects.all()
+    serializer_class = DeductionSerializer
+    permission_classes = [IsAuthenticated]
+    
+class GovernorateViewSet(viewsets.ModelViewSet):
+    queryset = Governorate.objects.all()
+    serializer_class = GovernrateSerializer
+    permission_classes = [IsAuthenticated]
+
+class JobTitleViewSet(viewsets.ModelViewSet):
+    queryset = JobTitle.objects.all()
+    serializer_class = JobTitleSerializer
+    permission_classes = [IsAuthenticated]
+    
+class JobTitleHistoryViewSet(viewsets.ModelViewSet):
+    queryset = JobTitleHistory.objects.all()
+    serializer_class = JobTitleHistorySerializer
+    permission_classes = [IsAuthenticated]
+
+class SalaryHistoryViewSet(viewsets.ModelViewSet):
+    queryset = SalaryHistory.objects.all()
+    serializer_class = SalaryHistorySerializer
+    permission_classes = [IsAuthenticated]
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
