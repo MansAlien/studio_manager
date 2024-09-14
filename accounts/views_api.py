c9700846a1a5861fcdba40800579b8b32f95c8f8
MansAlien <mans012033945421@gmail.com>
2024-09-14 16:33:32 +0300

    add the view permissions to the viewsets

--- accounts/views_api.py
+++ accounts/views_api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
 
 from .models import (
     City,
@@ -25,30 +26,56 @@ from .serializers import (
 )
 
 
+def check_permission(user, model_name, permission_type="view"):
+    """Check if the user has the specified permission for a given model."""
+    permission_codename = f"{permission_type}_{model_name.lower()}"
+    if not user.has_perm(f'accounts.{permission_codename}'):
+        raise PermissionDenied(f"You do not have permission to {permission_type} {model_name}.")
+
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
+
+    def get_queryset(self):
+            check_permission(self.request.user, 'city')
+            return super().get_queryset()
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+            check_permission(self.request.user, 'country')
+            return super().get_queryset()
+
 class DeductionViewSet(viewsets.ModelViewSet):
     queryset = Deduction.objects.all()
     serializer_class = DeductionSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+            check_permission(self.request.user, 'deduction')
+            return super().get_queryset()
     
 class GovernorateViewSet(viewsets.ModelViewSet):
     queryset = Governorate.objects.all()
     serializer_class = GovernorateSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
+
+    def get_queryset(self):
+            check_permission(self.request.user, 'governorate')
+            return super().get_queryset()
 
 class JobTitleViewSet(viewsets.ModelViewSet):
     queryset = JobTitle.objects.all()
     serializer_class = JobTitleSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+            check_permission(self.request.user, 'jobtitle')
+            return super().get_queryset()
     
 class JobTitleHistoryViewSet(viewsets.ModelViewSet):
     queryset = JobTitleHistory.objects.all()
@@ -63,9 +90,17 @@ class SalaryHistoryViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
+
+    def get_queryset(self):
+            check_permission(self.request.user, 'user')
+            return super().get_queryset()
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+            check_permission(self.request.user, 'userprofile')
+            return super().get_queryset()
